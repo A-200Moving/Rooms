d1a569943161a8eceeb6e9f65751278dcfae0f47
A-200 <124668668+A-200Moving@users.noreply.github.com>
2024-08-04 12:44:22 +0700

    EntityNodes.lua

---
+++ EntityNodes.lua
@@ -0,0 +1,22 @@
+
+local Part = Instance.new("Part", workspace)
+Part.Name = "EntityWalkPart"
+Part.Anchored = true
+Part.CFrame = workspace.start.exit.CFrame
+
+workspace.ChildAdded:Connect(function(room)
+   if room.Name == "tall room" or 
+      room.Name == "break room" or 
+      room.Name == "room 1" or 
+      room.Name == "room 2" or 
+      room.Name == "room 3" or 
+      room.Name == "room 4" or 
+      room.Name == "room 5" or  
+      room.Name == "box room" or 
+      room.Name == "reference room" or 
+      room.Name == "projector room" or 
+      room.Name == "locker room" or 
+      room.Name == "Storage" then
+      Part.CFrame = room:WaitForChild("door").CFrame
+   end
+end)
