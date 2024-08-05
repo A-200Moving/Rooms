3d0f1b478cf36e266ea2e28d3e1b6691a68f2cdc
A-200 <124668668+A-200Moving@users.noreply.github.com>
2024-08-05 14:43:19 +0700

    EntityModel.lua

--- EntityModel.lua
+++ EntityModel.lua
@@ -1,8 +1,5 @@
-local Entity = {}
-
-function Entity.Create(SpawnPart, Name, Face, CanChangeFace, HasLight, SoundId)
-  local Model = Instance.new("Model", workspace)
-Model.Name = Name
+local Model = Instance.new("Model", workspace)
+Model.Name = "A-60"
 local Main = Instance.new("Part", Model)
 Main.Name = "Main"
 Main.Anchored = true
@@ -14,48 +11,40 @@ BillboardGui.Size = UDim2.new(8,0,8,0)
 local Face = Instance.new("ImageLabel", BillboardGui)
 Face.Size = UDim2.new(1,0,1,0)
 Face.BackgroundTransparency = 1
-if CanChangeFace == true then
-  local function FACE_fake_script()
-	local script = Instance.new('LocalScript', Face)
+local function FACE_fake_script()
+  local script = Instance.new('LocalScript', Face)
 
-	while true do
-		script.Parent.Image = "rbxassetid://192267375"
-		wait(0.1)
-		script.Parent.Image = "rbxassetid://1972219027"
+  while true do
+    script.Parent.Image = "rbxassetid://192267375"
+    wait(0.1)
+    script.Parent.Image = "rbxassetid://1972219027"
     wait(0.1)
     script.Parent.Image = "rbxassetid://1822114127"
     wait(0.1)
-		script.Parent.Image = "rbxassetid://14084270847"
-		wait(0.1)
-		script.Parent.Image = "rbxassetid://3413871766"
+    script.Parent.Image = "rbxassetid://14084270847"
+    wait(0.1)
+    script.Parent.Image = "rbxassetid://3413871766"
     wait(0.1)
     script.Parent.Image = "rbxassetid://17865063"
     wait(0.1)
-	end
-  end
-  coroutine.wrap(FACE_fake_script)()
-else
-
+   end
 end
-if HasLight == true then
-  local Light = Instance.new("PointLight",Main)
+coroutine.wrap(FACE_fake_script)()
+local Light = Instance.new("PointLight",Main)
 Light.Color = Color3.fromRGB(255,0,0)
 Light.Brightness = 40
 Light.Range = 20
 Light.Shadows = true
-end
 local Sound = Instance.new("Sound",Main)
 Sound.Name = "Static"
 Sound.Looped = true
 Sound.PlaybackSpeed = 2
 Sound.Volume = 5
 Sound.EmitterSize = 25
-Sound.SoundId = SoundId
+Sound.SoundId = "rbxassetid://"
 Sound:Play()
 local DistortionSoundEffect = Instance.new("DistortionSoundEffect", Sound)
 DistortionSoundEffect.Level = 0.25
 local PitchShiftSoundEffect = Instance.new("PitchShiftSoundEffect",Sound)
 PitchShiftSoundEffect.Octave = 0.5
 end
-
-return Entity
