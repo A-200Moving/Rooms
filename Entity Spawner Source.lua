d574989cb67af30521b5cce6cf9d00b049583018
A-200 <124668668+A-200Moving@users.noreply.github.com>
2024-10-26 18:59:52 +0700

    Update Entity Spawner Source.lua

--- Entity Spawner Source.lua
+++ Entity Spawner Source.lua
@@ -1,5 +1,6 @@
 
-local Speed = 300
+local Speed = 50
+local Hitbox = 30
 
 
 
@@ -18,6 +19,34 @@ Part.CFrame = workspace.start.door.CFrame
 Part.Parent = workspace
 Part.CanCollide = false
 Part.Transparency = 1
+local Sound = Instance.new("Sound")
+Sound.PlaybackSpeed = 3.5
+Sound.Volume = 1
+Sound.Looped = true
+Sound.SoundId = "https://www.roblox.com/asset/?id=9126159606"
+Sound.Parent = Part
+local distort = Instance.new("DistortionSoundEffect")
+distort.Parent = Sound
+distort.Level = 0.95
+local equalizer = Instance.new("EqualizerSoundEffect")
+equalizer.Parent = Sound
+equalizer.HighGain = -37.7
+equalizer.LowGain = 10
+equalizer.MidGain = -2.7
+Sound:Play()
+local Face = Instance.new("BillboardGui")
+Face.Size = UDim2.new(7,0,7,0)
+Face.Parent = Part
+local Image = Instance.new("ImageLabel")
+Image.Size = UDim2.new(1,0,1,0)
+Image.BackgroundTransparency = 1
+Image.Image = "rbxassetid://15146660639"
+Image.Parent = Face
+local PL = Instance.new("PointLight")
+PL.Brightness = 15
+PL.Range = 20
+PL.Color = Color3.new(0,1,1)
+PL.Parent = Part
 
 if workspace:FindFirstChild("Waypoints") then
    for _,v in pairs(workspace:FindFirstChild("Waypoints"):GetChildren()) do
@@ -27,6 +56,29 @@ end
 
 workspace.Waypoints.ChildAdded:Connect(function(v)
    table.insert(Waypoints, v)
+end)
+
+spawn(function()
+loop = nil 
+loop = game:GetService("RunService").RenderStepped:connect(function()
+   if Part and Part.Parent then
+      local plr = game:GetService("Players").LocalPlayer
+   local Char = plr.Character
+   local hum = Char:FindFirstChild("Humanoid")
+   local rootpart = Char:FindFirstChild("HumanoidRootPart")
+  if hum and rootpart then
+     if Char:FindFirstChild("safe") then
+         if Char.safe.Value == 0 and hum.Health > 0 then
+             if (rootpart.Position - Part.Position).Magnitude <= Hitbox then
+                  hum:TakeDamage(100)
+             end
+         end
+     end
+  end
+   else
+      loop:Disconnect()
+   end
+end)
 end)
 
 while wait() do
@@ -43,5 +95,4 @@ while wait() do
    end
 break
 end
-
 Part:Destroy()
